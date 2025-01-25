68570887447f9ad336f21326c5c89541a1ccc5b1
Calum Knott <calum@calumk.com>
2025-01-25 18:19:57 +0000

    quick fixes

--- dev/example_data.js
+++ dev/example_data.js
@@ -1,29 +1,70 @@
 let example_data = {
 	blocks: [
-		{
-			type: "header",
-			data: {
-				text: "Example : @calumk/editorjs-codecup ",
-				level: 3,
-			},
-		},
-		{
-			type: "paragraph",
-			data: {
-				text: "This is an example of using EditorJs, with the @calumk/editorjs-media package",
-			},
-		},
-		{
-			type: "media",
-			data: {
-				file: {
-					url: "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg"
+				{
+					"id" : "zKmOieeS7g",
+					"type" : "header",
+					"data" : {
+						"text" : "Example : @calumk/editorjs-media-asset",
+						"level" : 3
+					}
 				},
-				caption: "Roadster // tesla.com",
-				withBorder: false,
-				withBackground: false,
-				stretched: true
-			}
-		}	
+				{
+					"id" : "pNlZ26fxaO",
+					"type" : "paragraph",
+					"data" : {
+						"text" : "This is an example of using EditorJs, with the @calumk/editorjs-media-asset package"
+					}
+				},
+				{
+					"id" : "7PPyvE_KCg",
+					"type" : "paragraph",
+					"data" : {
+						"text" : "This is an example of a file download :"
+					}
+				},
+				{
+					"id" : "4ATd90hfol",
+					"type" : "media",
+					"data" : {
+						"file" : {
+							"url" : "http://localhost:56738/dev/testfiles/1.pdf"
+						},
+						"caption" : "This is a file :)"
+					}
+				},
+				{
+					"id" : "9mD5ayC_0Z",
+					"type" : "paragraph",
+					"data" : {
+						"text" : "This is an example of a video :&nbsp;"
+					}
+				},
+				{
+					"id" : "Q3_MVDGIXi",
+					"type" : "media",
+					"data" : {
+						"file" : {
+							"url" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+						},
+						"caption" : "Big Buck"
+					}
+				},
+				{
+					"id" : "7Nipd-PXd4",
+					"type" : "paragraph",
+					"data" : {
+						"text" : "This is an example of an image"
+					}
+				},
+				{
+					"id" : "Y8Dlcng67y",
+					"type" : "media",
+					"data" : {
+						"file" : {
+							"url" : "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg"
+						},
+						"caption" : "Roadster // tesla.com"
+					}
+				}
 	],
 };
